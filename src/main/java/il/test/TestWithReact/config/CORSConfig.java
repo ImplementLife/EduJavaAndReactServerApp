14be58508aa8b05caae1019a15bb52d3aa9fa484
ImplementLife <implementlifep@gmail.com>
2024-06-15 00:50:51 +0300

    some little changes

--- src/main/java/il/test/TestWithReact/config/CORSConfig.java
+++ src/main/java/il/test/TestWithReact/config/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*")
                     .allowCredentials(true);
             }
--- src/main/java/il/test/TestWithReact/config/StaticResConfig.java
+++ src/main/java/il/test/TestWithReact/config/StaticResConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class StaticResConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
